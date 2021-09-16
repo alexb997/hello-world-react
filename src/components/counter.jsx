6d49fe24df7a01a6af14395616b0aba46f4c2ff3
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-16 22:45:10 +0300

    using bootstrap classnames

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,14 @@ import React, { Component } from 'react'
 class Counter extends React.Component {
     state = {
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
     };
 
     render() { 
         return (<React.Fragment>
-                    <img src={this.state.imageUrl} alt=""/>
-                    <span>{this.formatCount()}</span>
+                    <span className="badge badge-primary m-2">{this.formatCount()}</span> 
                     <button>Increment</button>
                 </React.Fragment>);
-    }
+    }/*Badge is making the blue background and m-2 makes a margin */
 
     formatCount () {
         const { count } = this.state;
