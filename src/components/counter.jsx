7b504cde236858d67f635766e960c8de4eb6b429
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-16 22:47:12 +0300

    more bootstrap classnames

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,9 +8,10 @@ class Counter extends React.Component {
     render() { 
         return (<React.Fragment>
                     <span className="badge badge-primary m-2">{this.formatCount()}</span> 
-                    <button>Increment</button>
+                    <button className="btn btn-secondary btn-sm">Increment</button>
                 </React.Fragment>);
-    }/*Badge is making the blue background and m-2 makes a margin */
+    }/*Badge is making the blue background and m-2 makes a margin. 
+    Look into bootstrap documentation for more class names*/
 
     formatCount () {
         const { count } = this.state;
