b30a22c2e109cf758f5f46ed3e6250166aa67c88
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-16 21:39:01 +0300

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
 
 const element = <h1>Hello World</h1>;
-
-console.log(element);
+reactDom.render(element, document.getElementById('root'));
