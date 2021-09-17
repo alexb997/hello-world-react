e43dd730c2dc21a1a502179bb1f6acf620282390
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-17 10:53:27 +0300

    classes as a constant

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,15 +4,12 @@ class Counter extends React.Component {
     state = {
         count: 0,
     };
-
-    styles={
-        fontSize: 10,// will be converted automatically to 10px
-        fontWeight: "bold"
-    };
-
     render() { 
+        const classes ="badge m-2 badge-"
+        classes += (this.state.count===0) ? "warning" : "primary";
+
         return (<React.Fragment>
-                    <span style={ this.styles } className="badge badge-primary m-2">{this.formatCount()}</span> 
+                    <span className={classes}>{this.formatCount()}</span> 
                     <button className="btn btn-secondary btn-sm">Increment</button>
                 </React.Fragment>);
     }/*Badge is making the blue background and m-2 makes a margin. 
